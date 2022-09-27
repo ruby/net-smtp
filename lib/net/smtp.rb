79aeda9d707061ef90f060e6c27b362a6b1a86ea
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2022-09-27 14:08:11 +0900

    Use OpenSSL::Digest instead of digest

--- lib/net/smtp.rb
+++ lib/net/smtp.rb
@@ -18,7 +18,6 @@
 #
 
 require 'net/protocol'
-require 'digest/md5'
 require 'timeout'
 begin
   require 'openssl'
@@ -888,14 +887,14 @@ module Net
 
     # CRAM-MD5: [RFC2195]
     def cram_md5_response(secret, challenge)
-      tmp = Digest::MD5.digest(cram_secret(secret, IMASK) + challenge)
-      Digest::MD5.hexdigest(cram_secret(secret, OMASK) + tmp)
+      tmp = OpenSSL::Digest::MD5.digest(cram_secret(secret, IMASK) + challenge)
+      OpenSSL::Digest::MD5.hexdigest(cram_secret(secret, OMASK) + tmp)
     end
 
     CRAM_BUFSIZE = 64
 
     def cram_secret(secret, mask)
-      secret = Digest::MD5.digest(secret) if secret.size > CRAM_BUFSIZE
+      secret = OpenSSL::Digest::MD5.digest(secret) if secret.size > CRAM_BUFSIZE
       buf = secret.ljust(CRAM_BUFSIZE, "\0")
       0.upto(buf.size - 1) do |i|
         buf[i] = (buf[i].ord ^ mask).chr
